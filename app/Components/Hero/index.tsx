66a8c1d430b15f781d71142c79004771b5967723
Syed Nabeel Sharafat <nabeelsharafat@gmail.com>
2024-05-19 02:59:27 +0500

    hero content changed

--- app/Components/Hero/index.tsx
+++ app/Components/Hero/index.tsx
@@ -3,12 +3,12 @@ export function Hero() {
   return (
     <div className="flex flex-col items-center justify-between space-y-2 max-w-5xl mx-auto">
       <h1 className="mx-4 md:text-5xl text-4xl text-center font-extrabold text-wrap">
-        Your Ultimate Travel Packing Assistant
+        Essential Gear, Perfectly Packed
       </h1>
       <p className="mx-4 md:text-xl text-center text-wrap">
-        Say goodbye to packing stress! PackMyTrip organizes your gear
-        efficiently, ensuring you have everything you need for an unforgettable
-        trip.
+        Planning a trip? PackMyTrip ensures you don’t miss a thing. Get a custom
+        packing list tailored to your itinerary. Pack with confidence using
+        PackMyTrip.
       </p>
       <Image
         src="./hero.svg"
